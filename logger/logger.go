7e8f3969fb71eef2a9791077b3b580d8b53723ab
weikeduopp <1647090769@qq.com>
2020-12-29 18:21:24 +0800

    update logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,9 +1,13 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+// DEBUG... level
 const (
-	DEBUG = iota
+	DEBUG LogLevel = iota
 	TRACE
 	INFO
 	WARNING
@@ -11,8 +15,12 @@ const (
 	FATAL
 )
 
+// LogLevel log级别
+type LogLevel uint8
+
 // Logger 日志结构体
 type Logger struct {
+	level []LogLevel
 }
 
 // NewLog Logger 构造函数
@@ -20,7 +28,40 @@ func NewLog() Logger {
 	return Logger{}
 }
 
+// checkLevel 检测是否包含
+func (l Logger) checkLevel(level LogLevel) (b bool) {
+	for _, v := range l.level {
+		if v == level {
+			b = true
+		}
+	}
+	return
+}
+
+// pln 公用
+func (l Logger) pln(msg interface{}, level LogLevel) {
+	now := time.Now()
+	var levelInfo string
+	switch level {
+	case DEBUG:
+		levelInfo = "DEBUG"
+	case TRACE:
+		levelInfo = "TRACE"
+	case INFO:
+		levelInfo = "INFO"
+	case WARNING:
+		levelInfo = "WARNING"
+	case ERROR:
+		levelInfo = "ERROR"
+	case FATAL:
+		levelInfo = "FATAL"
+	default:
+		levelInfo = "unknown"
+	}
+	fmt.Printf("[%s] [%s] %v \n", now.Format("2006-01-02 15:04:05"), levelInfo, msg)
+}
+
 // Debug 调试
 func (l Logger) Debug(msg interface{}) {
-	fmt.Println(msg)
+	l.pln(msg, DEBUG)
 }

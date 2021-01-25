96e2411144a0d80e201535a332952895ace0da25
weikeduopp <1647090769@qq.com>
2021-01-25 18:56:43 +0800

    gomaxprocs

--- 21.goroutine/main.go
+++ 21.goroutine/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -13,10 +14,13 @@ var wg sync.WaitGroup
 func _func(i int) {
 	fmt.Println("func", i)
 }
+
 func _func2(i int) {
+	// 执行完 Done
 	defer wg.Done()
 	fmt.Println("func", i)
 }
+
 func _goroutine() {
 	fmt.Println()
 	fmt.Println("goroutine:")
@@ -44,9 +48,35 @@ func _rand() {
 	}
 }
 
+// m:n 把m个goroutine分配给n个操作线程去执行
+// gomaxprocs 进程(CPU逻辑核心数)  runtime.GOMAXPROCS()函数设置当前程序并发时占用的CPU逻辑核心数  GOMAXPROCS是m:n调度中的n
+
+func _procs() {
+	runtime.GOMAXPROCS(2) // 将逻辑核心数设为2，此时两个任务并行执行
+	wg.Add(2)
+	go a()
+	go b()
+	wg.Wait()
+}
+
+func a() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("A", i)
+	}
+	wg.Done()
+}
+
+func b() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("B", i)
+	}
+	wg.Done()
+}
+
 func main() {
 	_goroutine()
 	// main 就是一个goroutine.  main结束了. 其中创建的goroutine也会结束
 	_rand()
 	_wait()
+	_procs()
 }
